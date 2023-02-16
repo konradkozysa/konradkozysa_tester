21faa12f270b5c05497933e58d6331c1c487be27
Konrad Kozysa <kozysa.k@gmail.com>
2023-02-16 12:13:15 +0100

    0.8

---
+++ nauka-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Konrad";
+        int myAge = 35;
+        double myHeight = 1.72;
+        String myChild = "Jakub";
+        boolean haveWife = true;
+        char oneSign = 'K';
+        System.out.println(name);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(myChild);
+        System.out.println(haveWife);
+        System.out.println(oneSign);
+    }
+}
