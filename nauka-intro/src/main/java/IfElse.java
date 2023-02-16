283cd9ed46b7ce7b3aa979a4140953bf8b98332b
Konrad Kozysa <kozysa.k@gmail.com>
2023-02-16 13:19:27 +0100

    0.11 new

--- nauka-intro/src/main/java/IfElse.java
+++ nauka-intro/src/main/java/IfElse.java
@@ -1,6 +1,6 @@
 public class IfElse {
     public static void main(String[] args) {
-        int age = 65;
+        /*int age = 65;
         String below18 = "Nie mozesz pracowac, jestes za mlody";
         String under18below65 = "Mozesz pracowac";
         String under65 = "Jestes za stary na prace";
@@ -11,6 +11,34 @@ public class IfElse {
             System.out.println(under18below65);
         } else {
             System.out.println(under65);
+        }*/
+
+        char operation = 'a';
+        int a = 10;
+        int b = 5;
+
+        if (operation == '+') {
+            int display = sum(a, b);
+            System.out.println(display);
+        } else if (operation == '-') {
+            int display = sub(a, b);
+            System.out.println(display);
+        } else {
+            error();
         }
+    }
+
+    public static int sum(int a, int b) {
+        int result = a + b;
+        return result;
+    }
+
+    public static int sub(int a, int b) {
+        int result = a - b;
+        return result;
+    }
+
+    public static void error() {
+        System.out.println("Zla operacja!");
     }
 }
